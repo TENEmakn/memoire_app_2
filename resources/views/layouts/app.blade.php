b44954ca212d2aa9f983ad4cea49b9601eebb688
Tenemakan <143546432+Tenemakan@users.noreply.github.com>
2025-04-26 23:19:54 +0000

    Update app.blade.php_vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-    @vite('resources')
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <link rel="stylesheet" href="{{ asset('css/alerts.css') }}">
